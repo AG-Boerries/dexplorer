87b963ad5fd104d3740eb4f5156bacf046e38d44
LangeTo <tobias.hundertmark@dkfz-heidelberg.de>
2026-02-25 15:58:18 +0100

    Adding pre-prepared public data for default run of DExploreR.

--- R/runDExploreR.R
+++ R/runDExploreR.R
@@ -1,7 +1,7 @@
 #' @title Run DExploreR Shiny App
 #'
 #' @description
-#' ...
+#' This functions runs the DExploreR Shiny app. It sets up a python environment, if not existing, using \code{\link{configurePyEnv}()}, mounts user specified assets and data directories and finally starts DExploreR using \code{\link[shiny]{runApp}()} on "0.0.0.0:1234". If running without any parameters, it uses the data and assets provided with this package form GSE273186.
 #'
 #' @param asset_dir A directory containing the assets for DExploreR, such as logos.
 #'
@@ -11,13 +11,22 @@
 #'
 #' @export
 runDExploreR <- function(
-  asset_dir = "/Users/tobiashundertmark/Downloads",
-  data_dir = "/Users/tobiashundertmark/Downloads/data",
+  asset_dir = NULL,
+  data_dir = NULL,
   env_path = NULL
 ) {
+  # If no directories provided, use the default data and assets provided with this package
+  if (is.null(asset_dir)) {
+    asset_dir <- system.file("extdata", "assets", package = "dexplorer")
+  }
+  if (is.null(data_dir)) {
+    data_dir <- system.file("extdata", "data", package = "dexplorer")
+  }
+
   asset_dir <- normalizePath(asset_dir, mustWork = TRUE)
   data_dir <- normalizePath(data_dir, mustWork = TRUE)
 
+  # Configure python environment if not provided
   if (is.null(env_path)) {
     env_path <- configurePyEnv()
   }
@@ -27,22 +36,27 @@ runDExploreR <- function(
     required = TRUE
   )
 
+  # Add further assets contained in the package
   addResourcePath(
     "www",
     system.file("dexplorer_assets", package = "dexplorer")
   )
 
+  # Add the color palettes as resources
   addResourcePath(
     "color_palettes",
     system.file("dexplorer_assets/color_palettes", package = "dexplorer")
   )
 
+  # Add the user-specified assets
   addResourcePath("assets", asset_dir)
 
+  # Create a config list to pass to the server function
   config <- list(
     data_dir = data_dir
   )
 
+  # Create the Shiny app
   app <- shinyApp(
     ui = app_ui(),
     server = function(input, output, session) {
@@ -50,5 +64,7 @@ runDExploreR <- function(
     },
     onStart = app_startup
   )
+
+  # Run the app on "0.0.0.0:1234"
   runApp(app, host = "0.0.0.0", port = 1234)
 }
