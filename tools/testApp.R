8813a9bf8fbf8615695ac2f1eacb1128a5778ac7
LangeTo <tobias.hundertmark@dkfz-heidelberg.de>
2026-03-02 15:07:43 +0100

    DExploreR can now be run in three different modes.

---
+++ tools/testApp.R
@@ -0,0 +1,15 @@
+# This runs DExploreR with the internal data set provided with the package
+runDExploreR()
+
+# This runs DExploreR with a custom data set, meeting the apps's data formatting requirements
+d <- readRDS(paste0(
+  system.file("extdata", "data", package = "dexplorer"),
+  "/GSE273186.rds"
+))
+runDExploreR(data = d)
+
+
+# This runs DExploreR with a directory containing `.rds` and `.csv` files to select from different data sets
+data_dir <- system.file("extdata", "data", package = "dexplorer")
+asset_dir <- system.file("extdata", "assets", package = "dexplorer")
+runDExploreR(data = data_dir, asset_dir = asset_dir)
