f57c3244ce052e813048c9da68bd254da8f86b5c
Maximilian von der Linde <maximilian.linde@uniklinik-freiburg.de>
2026-06-01 11:42:30 +0200

    Update appUI version display

--- R/appUI.R
+++ R/appUI.R
@@ -88,7 +88,7 @@ app_ui <- function(config) {
           )
         ),
         div(
-          "DExploreR v0.8.1.0",
+          "DExploreR v0.8.1.4",
           class = "app-version-fixed"
         )
       ),
